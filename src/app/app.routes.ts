05b7942d66cf50f2cf6d8bbd9e56f92afa7c76fa
Amar Omerovic <amaromerovic473@gmail.com>
2025-01-08 11:52:41 +0100

    Add splash and launcher icon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'splash', component: SplashScreenComponent }, 
+    { path: 'welcome', component: WelcomeScreenComponent },
+    { path: '', redirectTo: '/splash', pathMatch: 'full' },  // Default route
+];
---
+++ src/app/splash-screen/splash-screen.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splash-screen',
+  imports: [CommonModule],
+  templateUrl: './splash-screen.component.html',
+  styleUrl: './splash-screen.component.scss'
+})
+export class SplashScreenComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.router.navigate(['/welcome']);
+    }, 3000);
+  }
+}
